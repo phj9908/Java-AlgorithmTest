a9c7ae0b3b6601eb4597bfa6c5875557e9be7b69
HyeJi <phj9908@gmail.com>
2022-07-01 16:41:23 +0900

    0701

---
+++ Programmers/src/java0701_1.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public double solution(int[] arr) {
+        return Arrays.stream(arr).average().orElse(0); // orElse(0) : average()의 리턴값이 null일 때 0을 반환하도록 
+        // or (double)(Arrays.stream(arr).sum())/arr.length
+    }
+
+//	  결과 확인 테스트
+//    public static void main(String[] args) {
+//    	int[] x= {1,2,3};
+//    	Solution sol = new Solution();
+//    	
+//    	System.out.print(sol.solution(x));	
+//    }
+}
