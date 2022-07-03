83f3836b0a16012292b3838366809c52bb58d3a2
HyeJi <phj9908@gmail.com>
2022-07-03 23:57:58 +0900

    0703

--- Programmers/src/Programmers/java0703_4.java
+++ Programmers/src/Programmers/java0703_4.java
@@ -1,5 +1,23 @@
+// 음양더하기
+
 package Programmers;
 
-public class java0703_4 {
-
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        
+        for(int i=0;i<absolutes.length;i++) {
+        	if(signs[i]) {
+        		answer+=absolutes[i];
+        	}else {
+        		answer-=absolutes[i];
+        	}
+        }
+//        // Or 삼항연산자 이용
+//        for (int i = 0; i < absolutes.length; i++) {
+//            answer += (signs[i]) ? absolutes[i] : -absolutes[i];
+//        }
+        
+        return answer;
+    }
 }
--- Programmers/src/Programmers/java0703_5.java
+++ Programmers/src/Programmers/java0703_5.java
@@ -1,5 +1,37 @@
+// 크레인 인형뽑기 게임
+
 package Programmers;
 
-public class java0703_5 {
+import java.util.*;
 
+class java0703_5 {
+	
+	public int solution(int[][] board, int[] moves) {
+		int answer = 0;
+
+		Stack<Integer> stack = new Stack<>();
+
+		for (int m : moves) {
+			m -= 1;
+
+			Loop1: 
+			for (int i = 0; i < board.length; i++) {
+
+				if (board[i][m] != 0) {
+
+					if (!stack.isEmpty() && stack.peek()== board[i][m]) {
+						answer += 2;
+						stack.pop();
+
+					} else {
+						stack.add(board[i][m]);
+					}
+					board[i][m] = 0;
+					break Loop1; 			// 특정 반복문 중지
+				}
+			}
+		}
+
+		return answer;
+	}
 }
