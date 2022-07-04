32a5f1a769a5deaffcf6b4d698fbde065a593ef1
HyeJi <phj9908@gmail.com>
2022-07-04 17:45:53 +0900

    0704

--- Programmers/src/Programmers/java0703_6.java
+++ Programmers/src/Programmers/java0703_6.java
@@ -7,11 +7,11 @@ package Programmers;
 class java0703_6 {
 
 	public int answer = 0; // 클래스 내의 어느 함수에서도 접근가능하도록
-	public boolean[] sieve;
+	public boolean[] sieve; // 주의) 여기서 = new boolean[n] 하면 안됨 main에서 해야함
 
 	public int solution(int[] nums) {
 
-		sieve = new boolean[3000];
+		sieve = new boolean[3001]; 		// 각 원소의 최댓값이 1000, 근데 3개 조합이기에 3000까지 소수 판별 해놔야 함
 
 		isPrime(); // sieve 인덱스 숫자의 소수 여부 판별
 
@@ -28,7 +28,7 @@ class java0703_6 {
 		}
 
 		for (int i = 2; i <= (int) Math.sqrt(3000); i++) {
-			for (int j = i * i; j <= 3000; j += i) {
+			for (int j = i * i; j <= 3000; j += i) { 		// i*i 미만은 이미 소수판단이 되었음
 				sieve[j] = false;
 			}
 		}
--- Programmers/src/Programmers/java0704_1.java
+++ Programmers/src/Programmers/java0704_1.java
@@ -1,5 +1,28 @@
+// 완주하지 못한 선수
+// HashMap
+
 package Programmers;
 
-public class java0704_1 {
+import java.util.HashMap;
 
+public class java0704_1 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = "";
+        
+        HashMap<String,Integer> map = new HashMap<>();
+        
+        // 초기화
+        for(String c: participant) 
+        	map.put(c,map.getOrDefault(c, 0)+1); // map.get(c) 에 값이 있다면 기존값, 값이 없었다면 디폴트로 0 
+        
+        for(String c: completion) 
+        	map.put(c, map.get(c)-1);
+                
+        for(String key:map.keySet()) {
+        	if(map.get(key)>0) 
+        		answer=key;
+        }
+        
+        return answer;
+    }
 }
