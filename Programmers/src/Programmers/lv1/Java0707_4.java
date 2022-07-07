9983b5e4a8d2140f2491ad6bd33b7626b8d3f994
HyeJi <phj9908@gmail.com>
2022-07-07 22:47:35 +0900

    0707

--- Programmers/src/Programmers/lv1/Java0707_4.java
+++ Programmers/src/Programmers/lv1/Java0707_4.java
@@ -1,5 +1,53 @@
+// 문자열 내림차순 정렬
 package Programmers.lv1;
 
-public class Java0707_4 {
+import java.util.Arrays;
 
+// StringBuilder 활용
+public class Java0707_4 {
+    public String solution(String s) {
+        String answer = "";
+        
+        char[] arr = s.toCharArray(); 
+        Arrays.sort(arr);
+        
+        StringBuilder sb = new StringBuilder(new String(arr));
+        // String의 경우 new StringBuilder(String)
+        // char[]의 경우 new StringBuilder(new String(char[])
+        
+        answer = sb.reverse().toString();
+        
+        return answer;
+    }
+}
+
+// Character[]에 나눠 담아 Arrays.sort() 활용
+public class Java0707_4 {
+    public String solution(String s) {
+        String answer = "";
+        
+        Character[] arr = new Character[s.length()]; 
+        for(int i=0; i<arr.length; i++) arr[i] = s.charAt(i);
+        
+        Arrays.sort(arr,Collections.reverseOrder());
+        // 이때 arr는 char[] 이 아닌 Character[] 이어야 함
+        
+        for(int i=0;i<arr.length;i++) answer+=arr[i];
+        
+        return answer;
+    }
+}
+
+// String[]에 나눠 담아 Arrays.sort() 활용
+public class Java0707_4 {
+ public String solution(String s) {
+     String answer = "";
+     
+     String[] arr = s.split("");
+     Arrays.sort(arr,Collections.reverseOrder());
+     
+     for(int i=0;i<arr.length;i++) answer+=arr[i];
+     
+     return answer;
+ }
 }
--- Programmers/src/Programmers/lv1/Java0707_5.java
+++ Programmers/src/Programmers/lv1/Java0707_5.java
@@ -1,5 +1,20 @@
+// 최소공배수와 최대공약수
 package Programmers.lv1;
 
 public class Java0707_5 {
-
+	
+	public int gcd(int a, int b) {
+		if(a==0) return b;
+		
+		return gcd(b%a,a);
+	}
+	
+    public int[] solution(int n, int m) {
+        int[] answer = new int[2];
+        
+        answer[0] = gcd(n,m);
+        answer[1] = n*m/answer[0];
+        
+        return answer;
+    }
 }
