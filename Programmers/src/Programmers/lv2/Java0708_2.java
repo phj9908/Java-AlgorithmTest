a8662da203ce135ca1c4911a756f8acaf37196dd
HyeJi <phj9908@gmail.com>
2022-07-11 16:07:16 +0900

    0711

--- Programmers/src/Programmers/lv2/Java0708_2.java
+++ Programmers/src/Programmers/lv2/Java0708_2.java
@@ -13,9 +13,7 @@ public class Java0708_2 {
         	// or str[i] = numbers[i] + "";
         }
         
-        Arrays.sort(str,(a,b)->{
-        	return (b+a).compareTo(a+b);
-        });
+        Arrays.sort(str,(a,b)-> (b+a).compareTo(a+b));
         // 내림차순 정렬
         // 예) 30 과 3 을 정렬 할 때, 3 30 순서가 돼야 함
        	// 두 수 a,b를 순서를 번갈아가며 합친 수(330,303) 중 큰 값을 앞으로 정렬하겠다     
@@ -23,6 +21,26 @@ public class Java0708_2 {
         for(String s:str) answer+=s;
         
         return answer.charAt(0) == '0' ? "0":answer;
+        // answer.startsWith("0")? "0":answer;
         // {0,0,0,0...} 일 경우 0 출력
     }
 }
+
+// Stream 활용
+
+//import java.util.*;
+//import java.util.stream.*;
+//
+//public class Java0708_2 {
+//    public String solution(int[] numbers) {
+//        String answer = "";
+//        
+//        answer = IntStream.of(numbers)			// Integer의 배열 생성
+//        		.mapToObj(String::valueOf)		// 배열을 String으로 변경
+//        		.sorted((s1,s2) -> (s2+s1).compareTo(s1+s2))	// 내림파순으로 정렬
+//        		.collect(Collectors.joining());					// 배열의 원소를 하나의 문자열로 합치기
+//        
+//        return answer.startsWith("0")? "0":answer;
+//        // {0,0,0,0...} 일 경우 0 출력
+//    }
+//}
--- Programmers/src/Programmers/lv3/Java0706_3.java
+++ Programmers/src/Programmers/lv3/Java0706_3.java
@@ -14,24 +14,48 @@ public class Java0706_3 {
 		int end = budgets[budgets.length-1];
 		int mid = 0;
 		
+		
 		while (start <= end) {
 			mid = (start + end) / 2;
 			// 초기 상한액 = 최대 예산과 0의 중간값
+			int sum=0;
 			
 			for (int b : budgets) {
-				int x = 0;
-				x = (mid > b) ? b : mid;
 				// 상한액보다 작으면 b, 상한액보다 크면 answer
-				M -= x;
-
-				if (M < 0)
-					break;
+				sum += (mid > b) ? b : mid;
 			}
 
-			if (M < 0)
-				end = mid+1;
+			if (M < sum)
+				end = mid-1;
 			else
-				start = mid-1;
+				start = mid+1;
+				answer = mid;
+		}
+		return answer;
+	}
+}
+
+// Stream 활용
+
+public class Java0706_3 {
+	public int solution(int[] budgets, int M) {
+		
+		int answer=0;
+		int start =0;
+		int end = IntStream.of(budgets).max().OrElse(0);
+		
+		while (start <= end) {
+			final int  mid = (start + end) / 2; 
+			// final : Stream을 사용할 때 쓰이는 변수들은 가변적이지 않아야함
+			
+			int sum=IntStream.of(budgets)
+					.map( x -> Math.min(x,mid)) // 상한액이하만 더할 수 있도록
+					.sum();
+
+			if (sum>M)
+				end = mid-1;
+			else
+				start = mid+1;
 				answer = mid;
 		}
 		return answer;
